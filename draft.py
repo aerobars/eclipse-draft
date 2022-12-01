3c936b7cc5a007f4ff8cb3eff1958a84482f30cc
Adrian <adrianbyers@gmail.com>
2022-11-30 22:24:58 -0500

    Add basic python draft code

---
+++ draft.py
@@ -0,0 +1,4 @@
+import random
+factionname = ["Eridani Empire", "Hydran Process", "Planta", "Descendants of Draco", "Mechanema", "Orion Hegemony"]
+draftfact = random.sample(factionname, 2)
+print(str(draftfact))
