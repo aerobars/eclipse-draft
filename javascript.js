f55438b3d9d4b1abf1552c80378248a95ee54d53
Adrian <adrianbyers@gmail.com>
2023-03-28 21:56:35 -0400

    Add draft function code, updates of index and style

--- javascript.js
+++ javascript.js
@@ -1,14 +1,20 @@
-import java.util.Collections;
 const races = ['Eridani Empire', 'Hydran Progress', 'Planta', 'Descendants of Draco', 'Mechanema', 'Orion Hegemony'];
 let availableRaces = races;
 
-//shuffles available races and present first two as the draft choices
+//shuffles available races and present  two as the draft choices
 function draft() {
-  Collections.shuffle(availableRaces)
-  console.log(availableRaces)
+  let optionOne = availableRaces[Math.floor(Math.random() * availableRaces.length)];
+  do {
+    let optionTwo = availableRaces[Math.floor(Math.random() * availableRaces.length)];
+  } while (optionOne == optionTwo);
+  let choices = [optionOne, optionTwo];
+  return choices
 }
 
+//display draft options as two buttons
+
 //updates list of available races after player selects
-function raceUpdate() {
+function raceUpdate(choice) {
   //remove the chosen race from availableRaces array and return updated array
+  availableRaces.filter(race => !choice.includes(race))
 }
