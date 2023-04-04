76d478cc29453038b8490860454886b5c512ede9
Adrian <adrianbyers@gmail.com>
2023-04-03 21:42:26 -0400

    Add listner events to clear title heading and button, update index and css parent container

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 const races = ['Eridani Empire', 'Hydran Progress', 'Planta', 'Descendants of Draco', 'Mechanema', 'Orion Hegemony'];
 let availableRaces = races;
 
+const page = document.querySelector('#page');
+const start = document.querySelector('#start');
+const title = document.querySelector('#title');
+
+
 //shuffles available races and present  two as the draft choices
 function draft() {
   let optionOne = availableRaces[Math.floor(Math.random() * availableRaces.length)];
@@ -18,3 +23,12 @@ function raceUpdate(choice) {
   //remove the chosen race from availableRaces array and return updated array
   availableRaces.filter(race => !choice.includes(race))
 }
+
+
+
+function clear(){
+  page.removeChild(start);
+  page.removeChild(title);
+}
+
+start.addEventListener('click', clear)
