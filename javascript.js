23359508830ee967c2cd1ff5b8e1ccd2ee1e5d41
Adrian <adrianbyers@gmail.com>
2023-04-19 21:28:05 -0400

    Add draftRound function and event listeners

--- javascript.js
+++ javascript.js
@@ -7,36 +7,7 @@
 
 const races = ['Eridani Empire', 'Hydran Progress', 'Planta', 'Descendants of Draco', 'Mechanema', 'Orion Hegemony'];
 let availableRaces = races;
-let draftRound = 0;
-
-const page = document.querySelector('#page');
-const start = document.querySelector('#start');
-const title = document.querySelector('#title');
-
-function draftStart() {
-  page.removeChild(start);
-  page.removeChild(title);
-}
-
-//shuffles available races and present two as the draft choices
-function draftOptions() {
-  let optionOne = availableRaces[Math.floor(Math.random() * availableRaces.length)];
-  do {
-    let optionTwo = availableRaces[Math.floor(Math.random() * availableRaces.length)];
-  } while (optionOne === optionTwo);
-  let choices = [optionOne, optionTwo];
-  return choices
-}
-
-//display draft options as two buttons/divs
-
-//updates list of available races after player selects
-function raceUpdate(choice) {
-  //remove the chosen race from availableRaces array and return updated array
-  availableRaces.filter(race => !choice.includes(race))
-}
-
-start.addEventListener('click', draftStart);
+let roundCount = 0;
 
 const choiceOne = document.createElement('div');
 choiceOne.classList.add('choice');
@@ -45,8 +16,62 @@ choiceOne.classList.add('choice');
 const choiceTwo = document.createElement('div');
 choiceTwo.classList.add('choice');
 
-page.appendChild(choiceOne);
-page.appendChild(choiceTwo);
+const page = document.querySelector('#page');
+const start = document.querySelector('#start');
+const title = document.querySelector('#title');
 
+//starts the draft after clicking begin
+
+
+function draftStart() {
+  page.removeChild(start);
+  page.removeChild(title);
+
+  page.setAttribute('style', 'flex-direction: row')
+  draftRound();
+}
+
+//display draft options as two buttons/divs
+
+function draftRound() {
+  choiceOne.textContent = "";
+  choiceTwo.textContent = "";
+
+  let choices = draftOptions();
+  choiceOne.textContent = choices[0];
+  choiceTwo.textContent = choices[1];
+
+  page.appendChild(choiceOne);
+  page.appendChild(choiceTwo);
+}
+
+document.querySelectorAll('.choice').forEach((element) => {
+  element.addEventListener('click', () => {
+    element.setAttribute('style', 'background-color: red');
+    raceUpdate(element.textContent);
+    draftRound();
+  })
+})
+
+//shuffles available races and present two as the draft choices
+function draftOptions() {
+  let optionOne = availableRaces[Math.floor(Math.random() * availableRaces.length)];
+  let optionTwo;
+  do {
+    optionTwo = availableRaces[Math.floor(Math.random() * availableRaces.length)];
+  } while (optionOne === optionTwo);
+  let choices = [optionOne, optionTwo];
+  return choices
+}
+
+
+
+//updates list of available races after player selects
+function raceUpdate(choice) {
+  //remove the chosen race from availableRaces array and return updated array
+  availableRaces.filter(race => !choice.includes(race))
+}
+
+start.addEventListener('click', draftStart);
 
 
